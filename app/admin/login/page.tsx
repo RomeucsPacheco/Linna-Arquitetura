471fb48c4bc457df3ee828477efd957c9dd282fc
RomeucsPacheco <romeu.csp04@aluno.ifsc.edu.br>
2026-06-15 19:36:16 -0300

    Botão para mostrar senha

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -3,11 +3,13 @@
 import { useState} from 'react'
 import { supabase } from '@/lib/supabaseClient'
 import { useRouter} from 'next/navigation'
+import { Eye, EyeOff } from 'lucide-react'
 
 export default function LoginPage() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [error, setError] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
     const router = useRouter()
 
     const handleLogin = async (e: React.FormEvent) => {
@@ -51,13 +53,26 @@ return (
 
           <div>
             <label className="block text-sm font-medium text-gray-700">Senha</label>
-            <input
-              type="password"
-              required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 text-gray-700 rounded-md shadow-sm focus:outline-none focus:ring-black focus:border-black"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <div className="relative mt-1">
+              <input
+                type={showPassword ? "text" : "password"}
+                required
+                className="block w-full pr-10 pl-3 py-2 border border-gray-300 text-gray-700 rounded-md shadow-sm focus:outline-none focus:ring-black focus:border-black"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500 hover:text-gray-700 focus:outline-none"
+              >
+                {showPassword ? (
+                  <EyeOff className="h-5 w-5" aria-hidden="true" />
+                ) : (
+                  <Eye className="h-5 w-5" aria-hidden="true" />
+                )}
+              </button>
+            </div>
           </div>
 
           {error && <p className="text-red-500 text-sm text-center">{error}</p>}
